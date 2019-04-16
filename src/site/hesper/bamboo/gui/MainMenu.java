7e96722dd3e4882d72e32ba6895a3db7ff1e6ee7
satokadumasa <sato.kadumasa@gmail.com>
2019-04-17 01:01:50 +0900

    LayoutをGroupLayoutに変更

--- src/site/hesper/bamboo/gui/MainMenu.java
+++ src/site/hesper/bamboo/gui/MainMenu.java
@@ -1,8 +1,12 @@
 package site.hesper.bamboo.gui;
 
-import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Toolkit;
 
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -31,12 +35,29 @@ public class MainMenu extends JFrame {
 	 * Create the frame.
 	 */
 	public MainMenu() {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(0, 0, 500, screenSize.height);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JButton btnNewButton = new JButton("New button");
+		GroupLayout layout = new GroupLayout(contentPane);
+		layout.setHorizontalGroup(
+			layout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, layout.createSequentialGroup()
+					.addContainerGap(310, Short.MAX_VALUE)
+					.addComponent(btnNewButton)
+					.addGap(63))
+		);
+		layout.setVerticalGroup(
+			layout.createParallelGroup(Alignment.LEADING)
+				.addGroup(layout.createSequentialGroup()
+					.addGap(38)
+					.addComponent(btnNewButton)
+					.addContainerGap(981, Short.MAX_VALUE))
+		);
+//		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 	}
-
 }
